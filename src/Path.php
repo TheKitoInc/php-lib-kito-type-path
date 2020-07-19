c9d46c81fef144c8d848f0c2192f260c508cca94
TheKito <11139894+TheKito@users.noreply.github.com>
2020-07-19 13:30:16 -0300

    Update Path.php

--- src/Path.php
+++ src/Path.php
@@ -22,11 +22,13 @@ namespace Kito\Type;
  */
 class Path {
 
-    public static function getRoot(string $directorySeparator = DIRECTORY_SEPARATOR): Path {
+    public static function getRoot(string $directorySeparator = DIRECTORY_SEPARATOR): Path
+    {
         return new Path(array(), $directorySeparator);
     }
 
-    private static function parsePath(string $stringPath, string $directorySeparator = DIRECTORY_SEPARATOR): array {
+    private static function parsePath(string $stringPath, string $directorySeparator = DIRECTORY_SEPARATOR): array
+    {
         $tmp = array();
         foreach (explode($directorySeparator, str_replace("/", $directorySeparator, str_replace("\\", $directorySeparator, $stringPath))) as $name) {
             if (empty($name)) {
@@ -47,72 +49,87 @@ class Path {
         return $tmp;
     }
 
-    public static function getFromString(string $stringPath, string $directorySeparator = DIRECTORY_SEPARATOR): Path {
+    public static function getFromString(string $stringPath, string $directorySeparator = DIRECTORY_SEPARATOR): Path
+    {
         return new Path(self::parsePath($stringPath, $directorySeparator), $directorySeparator);
     }
 
     private $directorySeparator;
     protected $pathElements;
 
-    public function __construct(array $pathElements = array(), string $directorySeparator = DIRECTORY_SEPARATOR) {
+    public function __construct(array $pathElements = array(), string $directorySeparator = DIRECTORY_SEPARATOR)
+    {
         $this->directorySeparator = $directorySeparator;
         $this->pathElements = $pathElements;
     }
 
-    public function isRoot(): bool {
+    public function isRoot(): bool
+    {
         return $this->getDeep() == 0;
     }
 
-    public function getDeep(): int {
+    public function getDeep(): int
+    {
         return count($this->pathElements);
     }
 
-    public function getName(): string {
+    public function getName(): string
+    {
         return end($this->pathElements);
     }
 
-    public function setName(string $name): Path {
+    public function setName(string $name): Path
+    {
         array_pop($this->pathElements);
         $this->pathElements[] = $name;
         return $this;
     }
 
-    public function getChild(string $name): Path {
+    public function getChild(string $name): Path
+    {
         return new Path(array_merge($this->pathElements, array($name)), $this->directorySeparator);
     }
 
-    public function getParent(): ?Path {
+    public function getParent(): ?Path
+    {
         if ($this->isRoot()) {
             return null;
         }
         return new Path(array_slice($this->pathElements, 0, count($this->pathElements) - 1, true), $this->directorySeparator);
     }
 
-    public function __toString(): string {
+    public function __toString(): string
+    {
         return implode($this->directorySeparator, $this->pathElements);
     }
 
-    public function combine(Path $subPath): Path {
+    public function combine(Path $subPath): Path
+    {
         return new Path(array_merge($this->pathElements, $subPath->pathElements), $this->directorySeparator);
     }
 
-    public function getDirectorySeparator(): string {
+    public function getDirectorySeparator(): string
+    {
         return $this->directorySeparator;
     }
 
-    public function getElements(): array {
+    public function getElements(): array
+    {
         return $this->pathElements;
     }
 
-    public function getElement(int $index): string {
+    public function getElement(int $index): string
+    {
         return isset($this->pathElements[$index]) ? $this->pathElements[$index] : null;
     }
 
-    public function getUID(): string {
+    public function getUID(): string
+    {
         return implode('#', $this->pathElements);
     }
 
-    public function setDirectorySeparator(string $directorySeparator): Path {
+    public function setDirectorySeparator(string $directorySeparator): Path
+    {
         $this->directorySeparator = $directorySeparator;
         return $this;
     }
