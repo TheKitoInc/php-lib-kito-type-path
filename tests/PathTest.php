204c732847b23bf08dec5b7de5208f14cc23e48d
TheKito <11139894+TheKito@users.noreply.github.com>
2020-07-19 13:20:35 -0300

    setup rep

---
+++ tests/PathTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class PathTest extends TestCase {
+
+    public function testTrueIsTrue() {
+        $this->assertSame(true, true);
+    }
+    
+    
+
+}
