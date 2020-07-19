4da0da3eef88a67f2813e512afb9807ecf3663f8
TheKito <11139894+TheKito@users.noreply.github.com>
2020-07-19 14:09:51 -0300

    Update Path.php

--- src/Path.php
+++ src/Path.php
@@ -1,16 +1,13 @@
 <?php
 /**
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
+ * Path
+ * Path string parser and handler
+ * php version 7.1
+ 
+ * @category Strings
+ * @package Kito 
+ * @author TheKito <TheKito@blktech.com>
+ * @license @license http://opensource.org/licenses/gpl-license.php GNU Public License
  */
 namespace Kito\Type;
 
