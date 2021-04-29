ac55177a2aa46989a1fbdeff59ff7adcbf3c238b
TheKito <11139894+TheKito@users.noreply.github.com>
2021-04-29 00:17:26 -0300

    Create PathFactoryInterface.php

---
+++ src/PathFactoryInterface.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Path Factory Interface
+ * Path string parser and handler
+ * php version 7.1.
+ *
+ * @category Strings
+ *
+ * @author   TheKito <TheKito@blktech.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU GPL
+ *
+ * @link     https://github.com/TheKito/type-path
+ */
+
+namespace Kito\Path;
+
+/**
+ * Path Class.
+ */
+interface PathFactoryInterface
+{
+    /**
+     * Create Path object from path string and directory separator char
+     *
+     * @param string $stringPath         path string
+     * @param string $directorySeparator directory char separator
+     *
+     * @return PathInterface
+     */
+    public static function createPathFromString(string $stringPath = DIRECTORY_SEPARATOR, string $directorySeparator = DIRECTORY_SEPARATOR) : PathInterface;
+
+    /**
+     * Create root Path object with separator char
+     *
+     * @param string $directorySeparator directory char separator
+     *
+     * @return PathInterface
+     */
+    public static function createRootPath(string $directorySeparator = DIRECTORY_SEPARATOR): PathInterface;
+}
