5e58d8b0754cedf836a78da7ea8aed76f0bdbfce
TheKito <11139894+TheKito@users.noreply.github.com>
2021-04-29 00:25:14 -0300

    Create PathFactoryTest.php

---
+++ tests/PathFactoryTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Kito\Path\Path;
+use Kito\Path\PathFactory;
+use PHPUnit\Framework\TestCase;
+
+class PathFactoryTest extends TestCase
+{
+    public function testFactoryRootPath():void
+    {
+        $this->assertInstanceOf(Path::class, PathFactory::createRootPath());
+    }
+
+    public function testFactoryRootPathWithCustomPathSeparator():void
+    {
+        $this->assertInstanceOf(Path::class, PathFactory::createRootPath('-'));
+    }
+
+    public function testFactoryExamplePath():void
+    {
+        $this->assertInstanceOf(Path::class, PathFactory::createPathFromString('mypath'));
+    }
+
+    public function testFactoryExamplePathWithCustomPathSeparator():void
+    {
+        $this->assertInstanceOf(Path::class, PathFactory::createPathFromString('mypath', '-'));
+    }
+}
