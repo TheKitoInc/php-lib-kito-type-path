5ef66a1c5980c82527fe32b0ab0a895cde605d87
TheKito <blankitoracing@gmail.com>
2021-05-17 08:18:13 -0300

    style

--- tests/PathFactoryTest.php
+++ tests/PathFactoryTest.php
@@ -9,44 +9,44 @@ use PHPUnit\Framework\TestCase;
 class PathFactoryTest extends TestCase
 {
     const customDirectorySeparator = '-';
-    
+
     public function testFactoryRootPath(): void
     {
         $instance = PathFactory::createRootPath();
-        
+
         $this->assertInstanceOf(Path::class, $instance);
         $this->assertSame(DIRECTORY_SEPARATOR, $instance->getDirectorySeparator());
         $this->assertTrue($instance->isRoot());
-        $this->assertSame(0,$instance->getDeep());
+        $this->assertSame(0, $instance->getDeep());
     }
 
     public function testFactoryRootPathWithCustomPathSeparator(): void
     {
         $instance = PathFactory::createRootPath(self::customDirectorySeparator);
-        
+
         $this->assertInstanceOf(Path::class, $instance);
         $this->assertSame(self::customDirectorySeparator, $instance->getDirectorySeparator());
         $this->assertTrue($instance->isRoot());
-        $this->assertSame(0,$instance->getDeep());
+        $this->assertSame(0, $instance->getDeep());
     }
 
     public function testFactoryExamplePath(): void
     {
         $instance = PathFactory::createPathFromString('mypath');
-        
+
         $this->assertInstanceOf(Path::class, $instance);
         $this->assertSame(DIRECTORY_SEPARATOR, $instance->getDirectorySeparator());
         $this->assertFalse($instance->isRoot());
-        $this->assertSame(1,$instance->getDeep());
+        $this->assertSame(1, $instance->getDeep());
     }
 
     public function testFactoryExamplePathWithCustomPathSeparator(): void
-    {        
+    {
         $instance = PathFactory::createPathFromString('mypath', self::customDirectorySeparator);
-        
+
         $this->assertInstanceOf(Path::class, $instance);
         $this->assertSame(self::customDirectorySeparator, $instance->getDirectorySeparator());
         $this->assertFalse($instance->isRoot());
-        $this->assertSame(1,$instance->getDeep());
+        $this->assertSame(1, $instance->getDeep());
     }
 }
