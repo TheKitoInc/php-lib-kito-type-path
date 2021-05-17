701e81b23488fdc1b4063d846d5990aa682c3d8f
TheKito <11139894+TheKito@users.noreply.github.com>
2021-05-17 08:23:48 -0300

    Develop (#3)
     add more factory tests

--- tests/PathFactoryTest.php
+++ tests/PathFactoryTest.php
@@ -8,23 +8,45 @@ use PHPUnit\Framework\TestCase;
 
 class PathFactoryTest extends TestCase
 {
+    const customDirectorySeparator = '-';
+
     public function testFactoryRootPath(): void
     {
-        $this->assertInstanceOf(Path::class, PathFactory::createRootPath());
+        $instance = PathFactory::createRootPath();
+
+        $this->assertInstanceOf(Path::class, $instance);
+        $this->assertSame(DIRECTORY_SEPARATOR, $instance->getDirectorySeparator());
+        $this->assertTrue($instance->isRoot());
+        $this->assertSame(0, $instance->getDeep());
     }
 
     public function testFactoryRootPathWithCustomPathSeparator(): void
     {
-        $this->assertInstanceOf(Path::class, PathFactory::createRootPath('-'));
+        $instance = PathFactory::createRootPath(self::customDirectorySeparator);
+
+        $this->assertInstanceOf(Path::class, $instance);
+        $this->assertSame(self::customDirectorySeparator, $instance->getDirectorySeparator());
+        $this->assertTrue($instance->isRoot());
+        $this->assertSame(0, $instance->getDeep());
     }
 
     public function testFactoryExamplePath(): void
     {
-        $this->assertInstanceOf(Path::class, PathFactory::createPathFromString('mypath'));
+        $instance = PathFactory::createPathFromString('mypath');
+
+        $this->assertInstanceOf(Path::class, $instance);
+        $this->assertSame(DIRECTORY_SEPARATOR, $instance->getDirectorySeparator());
+        $this->assertFalse($instance->isRoot());
+        $this->assertSame(1, $instance->getDeep());
     }
 
     public function testFactoryExamplePathWithCustomPathSeparator(): void
     {
-        $this->assertInstanceOf(Path::class, PathFactory::createPathFromString('mypath', '-'));
+        $instance = PathFactory::createPathFromString('mypath', self::customDirectorySeparator);
+
+        $this->assertInstanceOf(Path::class, $instance);
+        $this->assertSame(self::customDirectorySeparator, $instance->getDirectorySeparator());
+        $this->assertFalse($instance->isRoot());
+        $this->assertSame(1, $instance->getDeep());
     }
 }
