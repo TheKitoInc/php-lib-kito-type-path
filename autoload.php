f3b6c936291d6ddad688bfbdde23e8c3a27ebf1f
TheKito <11139894+TheKito@users.noreply.github.com>
2024-05-27 22:30:11 -0300

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,4 +1,6 @@
 <?php
 
+require_once __DIR__.'/src/PathFactoryInterface.php';
+require_once __DIR__.'/src/PathFactory.php';
 require_once __DIR__.'/src/PathInterface.php';
 require_once __DIR__.'/src/Path.php';
