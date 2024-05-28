c5611446378c0c5b7c2e48c0cfc093a5aea19926
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-05-28 11:10:32 -0300

    remap path

--- autoload.php
+++ autoload.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/src/PathFactoryInterface.php';
-require_once __DIR__.'/src/PathFactory.php';
-require_once __DIR__.'/src/PathInterface.php';
-require_once __DIR__.'/src/Path.php';
+require_once __DIR__.'/src/Kito/Type/Path/PathFactoryInterface.php';
+require_once __DIR__.'/src/Kito/Type/Path/PathFactory.php';
+require_once __DIR__.'/src/Kito/Type/Path/PathInterface.php';
+require_once __DIR__.'/src/Kito/Type/Path/Path.php';
