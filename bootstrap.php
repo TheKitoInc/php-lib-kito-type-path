8656a7c32b0eae21aca4519b051238c2c9b21fa1
Kito Git Service <git@thekitoinc.com>
2024-05-29 15:22:12 +0000

    Sync

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,18 @@
 <?php
+spl_autoload_register(
+    function ($classNameSpace) {
+        $classNameSpace = explode(DIRECTORY_SEPARATOR, str_replace('/', DIRECTORY_SEPARATOR, str_replace("\\", DIRECTORY_SEPARATOR, $classNameSpace)));
 
-require_once __DIR__.'/src/Kito/Type/Path/PathFactoryInterface.php';
-require_once __DIR__.'/src/Kito/Type/Path/PathFactory.php';
-require_once __DIR__.'/src/Kito/Type/Path/PathInterface.php';
-require_once __DIR__.'/src/Kito/Type/Path/Path.php';
+        foreach ($classNameSpace as $key => $value) {
+            if(empty($value) || $value == '.' || $value == '..') {
+                unset($classNameSpace[$key]);
+            }
+        }
+
+        $classPath = __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $classNameSpace) . '.php';
+
+        if(file_exists($classPath)) {
+            require_once $classPath;
+	}
+    }
+);
