d6e5c50b9b6a15ef1c4b37e5c7d878b90713ce01
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-03 11:23:38 -0300

    Create PathInterface.php

---
+++ src/Kito/Type/PathInterface.php
@@ -0,0 +1,122 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Path
+ * Path string parser and handler
+ * php version 7.1.
+ *
+ * @category Strings
+ *
+ * @author   TheKito <TheKito@blktech.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU GPL
+ *
+ * @link     https://github.com/TheKito/type-path
+ */
+
+namespace Kito\Type;
+
+/**
+ * Path Interface.
+ */
+interface PathInterface
+{
+    /**
+     * Get current directory separator.
+     *
+     * @return string
+     */
+    public function getDirectorySeparator(): string;
+
+
+
+    /**
+     * Return path with new directory separator from current path.
+     *
+     * @param string $directorySeparator DirectorySeparator to be merged before current path
+     *
+     * @return PathInterface
+     */
+    public function withDirectorySeparator(string $directorySeparator): PathInterface;
+
+    /**
+     * Get elements of path.
+     *
+     * @return array
+     */
+    public function getElements(): array;
+
+    /**
+     * Get path element.
+     *
+     * @param int $index path element position
+     *
+     * @return string
+     */
+    public function getElement(int $index): ?string;
+
+    /**
+     * Get number of components of path.
+     *
+     * @return int
+     */
+    public function getDeep(): int;
+
+    /**
+     * Return if path it is empty.
+     *
+     * @return bool
+     */
+    public function isRoot(): bool;
+
+    /**
+     * Get name from last path element know as filename.
+     *
+     * @return string
+     */
+    public function getName(): string;
+
+    /**
+     * Set name from last path element know as filename.
+     *
+     * @param string $name element name
+     *
+     * @return Path
+     */
+    public function setName(string $name): PathInterface;
+
+    /**
+     * Get new path object with child element.
+     *
+     * @param string $name element name
+     *
+     * @return Path
+     */
+    public function getChild(string $name): PathInterface;
+
+    /**
+     * Get parent path object.
+     *
+     * @return Path
+     */
+    public function getParent(): ?PathInterface;
+
+    /**
+     * Return path as string using directory separator.
+     *
+     * @return string
+     */
+    public function __toString(): string;
+
+
+
+    /**
+     * Return new path with current path combined to new path.
+     *
+     * @param PathInterface $path path to be merged after current path
+     *
+     * @return PathInterface
+     */
+    public function withSuffixPath(\Kito\Type\Path\PathInterface $path): PathInterface;
+}
